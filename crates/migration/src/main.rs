ef92325b7e542b4b7f660e55c6cf7b59c416bab8
ebalo55 <12586380+ebalo55@users.noreply.github.com>
2026-02-10 17:43:54 +0100

    refactor: check for existing DATABASE_URL before setting it in main function

--- crates/migration/src/main.rs
+++ crates/migration/src/main.rs
@@ -4,17 +4,19 @@ use sea_orm_migration::prelude::*;
 async fn main() {
     dotenvy::dotenv().ok();
     unsafe {
-        std::env::set_var(
-            "DATABASE_URL",
-            format!(
-                "postgres://{}:{}@{}:{}/{}",
-                std::env::var("HORIZON_DATABASE_USER").unwrap(),
-                std::env::var("HORIZON_DATABASE_PASSWORD").unwrap(),
-                std::env::var("HORIZON_DATABASE_HOST").unwrap(),
-                std::env::var("HORIZON_DATABASE_PORT").unwrap(),
-                std::env::var("HORIZON_DATABASE_NAME").unwrap(),
-            ),
-        );
+        if !std::env::var("DATABASE_URL").is_ok() {
+            std::env::set_var(
+                "DATABASE_URL",
+                format!(
+                    "postgres://{}:{}@{}:{}/{}",
+                    std::env::var("HORIZON_DATABASE_USER").unwrap(),
+                    std::env::var("HORIZON_DATABASE_PASSWORD").unwrap(),
+                    std::env::var("HORIZON_DATABASE_HOST").unwrap(),
+                    std::env::var("HORIZON_DATABASE_PORT").unwrap(),
+                    std::env::var("HORIZON_DATABASE_NAME").unwrap(),
+                ),
+            );
+        }
     }
     cli::run_cli(migration::Migrator).await;
 }
